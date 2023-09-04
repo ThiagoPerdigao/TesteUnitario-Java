d4bd9fd9a4869d7820f9d954b409cd32d51f431c
ThiagoPerdigao <thiagovitor299@gmail.com>
2023-09-04 09:44:59 -0300

    livro e tests

---
+++ src/Livro.java
@@ -0,0 +1,82 @@
+package src;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private boolean emprestado;
+
+    public Livro(int id, String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.emprestado = false; 
+    }
+
+
+    
+
+
+    public void criarLivro(int novoId, String novoTitulo, String novoAutor) {
+        this.id = novoId;
+        this.titulo = novoTitulo;
+        this.autor = novoAutor;
+        
+    }
+    
+
+    public void emprestarLivro() {
+        if (emprestado) {
+            throw new IllegalStateException("O livro já está emprestado.");
+        }
+        this.emprestado = true;
+    }
+
+    public void retornarLivro() {
+        if (!emprestado) {
+            throw new IllegalStateException("O livro já foi retornado.");
+        }
+        this.emprestado = false;
+    }
+
+
+
+    // Método para verificar se o livro está emprestado
+    public boolean estaEmprestado() {
+        return emprestado;
+    }
+
+
+
+    // Getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setEmprestado(boolean emprestado) {
+        this.emprestado = emprestado;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
+
+
+
---
+++ test/testLivro.java
@@ -0,0 +1,48 @@
+package test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+import src.Livro;
+
+public class testLivro {
+
+
+    @Test
+    public void testCriarLivro() {
+        Livro livro = new Livro(1, "nada", "nada");
+
+        livro.criarLivro(1, "Moby Dick", "Herman Melville");
+        
+        assertEquals(1, livro.getId());
+        assertEquals("Moby Dick", livro.getTitulo());
+        assertEquals("Herman Melville", livro.getAutor());
+    }
+
+
+    @Test
+    public void testEmprestarLivro() {
+
+        Livro livro = new Livro(1, "Livro do Thiago", "Thiago");
+       
+        livro.emprestarLivro();
+        assertTrue(livro.estaEmprestado());
+
+    }
+
+     @Test
+    public void testRetornarLivro() {
+
+        Livro livro = new Livro(1, "Livro do Thiago", "Thiago");
+        livro.emprestarLivro();
+        livro.retornarLivro();
+        assertFalse(livro.estaEmprestado());
+
+    }
+
+    
+
+
+
+}
